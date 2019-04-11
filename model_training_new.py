ed02f1f819a8a99d91c13ef8260c01f16594a6bf
EshaaSood <36894500+EshaaSood@users.noreply.github.com>
2019-04-11 11:31:10 -0400

    Add files via upload

--- model_training_new.py
+++ model_training_new.py
@@ -196,73 +196,74 @@ def get_next_batch(index, dataset, classes, batch_size):
 
 # Code starts here
 # Read Folder_1
-D = np.zeros(shape = (l1, ROWS, COLS, 3))
-D_flip = np.zeros(shape = (l1, ROWS, COLS, 3))
 
-filename_1 = 'CPSC_8810/gossiping/'
+
+
+filename_1 = 'gossiping/'
 a1 = sorted(os.listdir(filename_1))
 l1 = len(a1)
 # Read Folder_2
 
-filename_2 = 'CPSC_8810/isolation/'
+filename_2 = 'isolation/'
 a2 = sorted(os.listdir(filename_2))
 l2 = len(a2)
 L2 = 4*l1 + l2
 
 # Read Folder_3
-filename_3 = 'CPSC_8810/laughing/'
+filename_3 = 'laughing/'
 a3 = sorted(os.listdir(filename_3))
 l3 = len(a3)
 L3 = 4*l1 + 4*l2 + l3
 
 # Read Folder_4
-filename_4 = 'CPSC_8810/pullinghair/'
+filename_4 = 'pullinghair/'
 a4 = sorted(os.listdir(filename_4))
 l4 = len(a4)
 L4 = 4*l1 + 4*l2 + 4*l3 + l4
 
 # Read Folder_5
-filename_5 = 'CPSC_8810/punching/'
+filename_5 = 'punching/'
 a5 = sorted(os.listdir(filename_5))
 l5 = len(a5)
 L5 = 4*l1 + 4*l2 + 4*l3 + 4*l4 + l5
 
 # Read Folder_6
-filename_6 = 'CPSC_8810/quarrel/'
+filename_6 = 'quarrel/'
 a6 = sorted(os.listdir(filename_6))
 l6 = len(a6)
 L6 = 4*l1 + 4*l2 + 4*l3 + 4*l4 + 4*l5 + l6
 
 # Read Folder_7
-filename_7 = 'CPSC_8810/slapping/'
+filename_7 = 'slapping/'
 a7 = sorted(os.listdir(filename_7))
 l7 = len(a7)
 L7 = 4*l1 + 4*l2 + 4*l3 + 4*l4 + 4*l5 + 4*l6 + l7
 
 # Read Folder_8
-filename_8 = 'CPSC_8810/stabbing/'
+filename_8 = 'stabbing/'
 a8 = sorted(os.listdir(filename_8))
 l8 = len(a8)
 L8 = 4*l1 + 4*l2 + 4*l3 + 4*l4 + 4*l5 + 4*l6 + 4*l7 + l8
 
 # Read Folder_9
-filename_9 = 'CPSC_8810/strangle/'
+filename_9 = 'strangle/'
 a9 = sorted(os.listdir(filename_9))
 l9 = len(a9)
 L9 = 4*l1 + 4*l2 + 4*l3 + 4*l4 + 4*l5 + 4*l6 + 4*l7 + 4*l8 + l9
 
 # Read Standford 40 Images
-filename_10 = 'CPSC_8810/JPEGImages/'
+filename_10 = 'stanford/'
 a10 = sorted(os.listdir(filename_10))
 l10 = len(a10)
 number = L9
-L10 = L9 + number
+L10 = 4*l1 + 4*l2 + 4*l3 + 4*l4 + 4*l5 + 4*l6 + 4*l7 + 4*l8 + 4*l9 + number
 
 # Initialize the dataset and label container
 D = np.zeros(shape = (4*(L9 + number), ROWS, COLS, 3))
+D_flip = np.zeros(shape = (L9 + number, ROWS, COLS, 3))
 labels = np.zeros(shape = (4*(L9 + number)))
 
-D[0:l1, :, :, :] = read_from_folder(filename_1)
+D[0:l1, :, :, :] = read_from_folder(filename_1, 1, 0)
 
 D_flip = flip_images(D[0:l1, :, :, :])
 D_noise = add_noise(D[0:l1, :, :, :], 0, 0.25)
@@ -278,7 +279,7 @@ labels[(2*l1):(3*l1)] = 1
 labels[(3*l1):(4*l1)] = 1
 
 
-D[(4*l1):L2, :, :, :] = read_from_folder(filename_2)
+D[(4*l1):L2, :, :, :] = read_from_folder(filename_2 , 1, 0)
 D_flip = flip_images(D[(4*l1):L2, :, :, :])
 D_noise = add_noise(D[(4*l1):L2, :, :, :], 0, 0.25)
 D_crop = crop(D[(4*l1):L2, :, :, :], 25, 25, 25, 25)
@@ -294,7 +295,7 @@ labels[L2 + (2*l2): L2 + (3*l2)] = 2
 
 
 
-D[(4*l2):L3, :, :, :] = read_from_folder(filename_3)
+D[(4*l2):L3, :, :, :] = read_from_folder(filename_3, 1 ,0)
 D_flip = flip_images(D[(4*l2):L3, :, :, :])
 D_noise = add_noise(D[(4*l2):L3, :, :, :], 0, 0.25)
 D_crop = crop(D[(4*l2):L3, :, :, :], 25, 25, 25, 25)
@@ -310,7 +311,7 @@ labels[L3 + (2*l3): L3 + (3*l3)] = 3
 
 
 
-D[(4*l3):L4, :, :, :] = read_from_folder(filename_4)
+D[(4*l3):L4, :, :, :] = read_from_folder(filename_4, 1, 0)
 D_flip = flip_images(D[(4*l3):L4, :, :, :])
 D_noise = add_noise(D[(4*l3):L4, :, :, :], 0, 0.25)
 D_crop = crop(D[(4*l3):L4, :, :, :], 25, 25, 25, 25)
@@ -327,7 +328,7 @@ labels[L4 + (2*l4): L4 + (3*l4)] = 4
 
 
 
-D[(4*l4):L5, :, :, :] = read_from_folder(filename_5)
+D[(4*l4):L5, :, :, :] = read_from_folder(filename_5, 1 , 0)
 
 D_flip = flip_images(D[(4*l4):L5, :, :, :])
 D_noise = add_noise(D[(4*l4):L5, :, :, :], 0, 0.25)
@@ -342,7 +343,7 @@ labels[L5:L5 + l5] = 5
 labels[L5 + l5:L5 + (2*l5)] = 5
 labels[L5 + (2*l5): L5 + (3*l5)] = 5
 
-D[(4*l5):L6, :, :, :] = read_from_folder(filename_6)
+D[(4*l5):L6, :, :, :] = read_from_folder(filename_6, 1, 0)
 
 D_flip = flip_images(D[(4*l5):L6, :, :, :])
 D_noise = add_noise(D[(4*l5):L6, :, :, :], 0, 0.25)
@@ -357,7 +358,7 @@ labels[L6:L6 + l6] = 6
 labels[L6 + l6:L6 + (2*l6)] = 6
 labels[L6 + (2*l6): L6 + (3*l6)] = 6
 
-D[(4*l6):L7, :, :, :] = read_from_folder(filename_7)
+D[(4*l6):L7, :, :, :] = read_from_folder(filename_7, 1, 0)
 
 D_flip = flip_images(D[(4*l6):L7, :, :, :])
 D_noise = add_noise(D[(4*l6):L7, :, :, :], 0, 0.25)
@@ -372,7 +373,7 @@ labels[L7:L7 + l7] = 7
 labels[L7 + l7:L7 + (2*l7)] = 7
 labels[L7 + (2*l7): L7 + (3*l7)] = 7
 
-D[(4*l7):L8, :, :, :] = read_from_folder(filename_8)
+D[(4*l7):L8, :, :, :] = read_from_folder(filename_8, 1, 0)
 D_flip = flip_images(D[(4*l7):L8, :, :, :])
 D_noise = add_noise(D[(4*l7):L8, :, :, :], 0, 0.25)
 D_crop = crop(D[(4*l7):L8, :, :, :], 25, 25, 25, 25)
@@ -386,7 +387,7 @@ labels[L8:L8 + l8] = 8
 labels[L8 + l8:L8 + (2*l8)] = 8
 labels[L8 + (2*l8): L8 + (3*l8)] = 8
 
-D[(4*l8):L9, :, :, :] = read_from_folder(filename_9)
+D[(4*l8):L9, :, :, :] = read_from_folder(filename_9, 1, 0)
 D_flip = flip_images(D[(4*l8):L9, :, :, :])
 D_noise = add_noise(D[(4*l8):L9, :, :, :], 0, 0.25)
 D_crop = crop(D[(4*l8):L9, :, :, :], 25, 25, 25, 25)
@@ -400,7 +401,7 @@ labels[L9:L9 + l9] = 9
 labels[L9 + l9:L9 + (2*l9)] = 9
 labels[L9 + (2*l9): L9 + (3*l9)] = 9
 
-D[(4*l9):L10, :, :, :] = read_nonbullying(filename_10, number)
+D[(4*l9):L10, :, :, :] = read_nonbullying(filename_10, number, 1, 0)
 
 
 D_flip = flip_images(D[(4*l9):L10, :, :, :])
@@ -577,7 +578,7 @@ for epoch in range(epochs):
     print("Iteration",iters,"Train Loss",train_loss[iters],"Train Acc",train_acc[iters]) 
     print("Iteration",iters,"Test Loss",test_loss[iters],"Test Acc",test_acc[iters]) 
     print("\n")
-   iters = iters + 1
+   
     
 np.savez('CPSC_8810/plots/10categoriesnew_plots.npz',name1 = train_acc, name2 = train_loss, name3 = test_acc, name4 = test_loss)
 
